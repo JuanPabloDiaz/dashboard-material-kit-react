bae7f3c89c7885d75e47ec7cc9fc17fcaf4fdba5
JuanPabloDiaz <juanchodis@hotmail.com>
2024-02-08 17:58:33 -0500

    Update account.js and package-lock.json

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 export const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
+  displayName: 'Juan Diaz',
+  email: 'demo@llm.com.co',
   photoURL: '/assets/images/avatars/avatar_25.jpg',
 };
